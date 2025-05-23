b5bdcc3af0c4ae3ccc896349082283345eee80d6
M.Rizqullah <mm.rizqullah@gmail.com>
2025-05-23 22:10:06 +0800

    Refactor App component to include filtering functionality and sidebar; remove Products component. Add navigation and category selection features with new styles. Update product data structure for star ratings.

--- src/Product/Products.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Products.css";
-
-const Products = () => {
-  return <div>Products</div>;
-};
-
-export default Products;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { BsFillBagFill } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ img, title, reviews, prevPrice, newPrice }) => {
+  return (
+    <>
+      <section className="card">
+        <img src={img} alt={title} className="card-img" />
+        <div className="card-details">
+          <h3 className="card-title">{title}</h3>
+          <section className="card-reviews">
+            <AiFillStar className="rating-star" />
+            <AiFillStar className="rating-star" />
+            <AiFillStar className="rating-star" />
+            <AiFillStar className="rating-star" />
+            <span className="total-reviews">{reviews}</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>{prevPrice}</del> {newPrice}
+            </div>
+            <div className="bag">
+              <BsFillBagFill className="bag-icon" />
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Card;

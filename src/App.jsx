125e96a007a327f319108a4dd5bc32fdf2f940a3
M.Rizqullah <mm.rizqullah@gmail.com>
2025-05-23 18:14:43 +0800

    Add react-icons dependency and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Navigator from "./Navigation/Nav";
+import Products from "./Product/Products";
+import Recommended from "./Recommended/Recommended";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navigator />
+      <Products />
+      <Recommended />
+    </>
+  );
 };
 
 export default App;
